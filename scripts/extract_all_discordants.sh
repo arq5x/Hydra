00bb8d14bbcbe243549f5412602ac20785a87260
Mike Lindberg <ml4wc@users.noreply.github.com>
2013-08-28 15:07:43 -0400

    Update extract_all_discordants.sh

--- scripts/extract_all_discordants.sh
+++ scripts/extract_all_discordants.sh
@@ -32,7 +32,6 @@ function add_final_bam {
 function extract_discordants {
 	samtools view -bF 0x040E -f 0x001 $1 > $1.disc.tmp.bam
 	samtools sort -m 1000000000 -n $1.disc.tmp.bam $1.disc.tmp.bam.qrysort
-	rm $1.disc.tmp.bam
 	extract_discordants.py -i $1.disc.tmp.bam.qrysort.bam $2
 }
 
