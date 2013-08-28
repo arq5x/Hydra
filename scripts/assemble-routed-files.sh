4f3eccfd2122293ba1f2f5c2856767fdd7dc0416
Mike Lindberg <ml4wc@users.noreply.github.com>
2013-08-28 17:56:11 -0400

    Update assemble-routed-files.sh

--- scripts/assemble-routed-files.sh
+++ scripts/assemble-routed-files.sh
@@ -1,40 +1,43 @@
+#!/bin/bash
+set -e
+
 if [ -z $3 ]
 then
-	echo "usage:$0 <file w/ list of routed files> <hydra config file> <number of threads>"
+	echo "usage:$0 <file w/ list of routed files> <hydra config file> <number of processes>"
 	exit
 fi
 
 CONFIG=$1
 ROUTED_FILES=$2
 PROCS=$3
-COUNTER=0
 INDEX=0
+
+function poll {
+    while [[ $curr_jobs -ge PROCS ]]
+    do
+	curr_jobs=$(jobs -p | wc -l)
+	sleep 1
+	wait $!
+    done
+}
 
 function add_next_assem {
     # if still jobs to do then add one
-    if [[ $COUNTER -lt PROCS ]]
+    curr_jobs=$(jobs -p | wc -l)
+    if [[ $curr_jobs -lt PROCS ]]
     then
-	COUNTER=$(($COUNTER+1))
-		hydra-assembler -config $CONFIG -routed ${assems_todo[$INDEX]} -maxMappings 300000 & 
+	hydra-assembler -config $CONFIG -routed ${assems_todo[$INDEX]} -maxMappings 300000 & 
 		INDEX=$(($INDEX+1))
-	else
-		wait
-		COUNTER=$(($COUNTER-$PROCS))
+    else
+	poll
     fi
 }
 
-function add_final_assem {
-    hydra-assembler -config $CONFIG -routed ${assems_todo[$INDEX]} -maxMappings 300000
-    wait
-}
 
 assems_todo=($(cat $ROUTED_FILES)) # places output into an array
 max_index=${#assems_todo[*]}-1
-while [[ $INDEX -lt $max_index ]]
+while [[ $INDEX -le $max_index ]]
 do
     add_next_assem
 done
-if [[ $INDEX -eq $max_index ]]
-    then
-    add_final_assem
-fi
+wait
